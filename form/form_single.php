c58f6ae29f054200e90a563cd3cad40897f8163d
DAUD <sayyeddaud@gmail.com>
2014-07-19 14:14:18 +0530

    Form Design Completed

---
+++ form/form_single.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<title>Single Form Submission</title>
+<body>
+	<div id="wrap">
+		<div class="content">
+			<img src="user.png" width=50 style="margin-top:10px;"/>
+			<h3 style="">Member Login</h3>
+			<form action="" methpd="post" style="padding:12px;">
+				<input type="text" name="username" value="" placeholder="Username" required/><br/>
+				<input type="password" name="username" value="" placeholder="Password" required/><br/>
+				<button type="submit" name="submit" class="button"/>Log In</button><br/>
+				<br/>
+				<a href="">Forget Password ?</a>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
